c6537526e31c41bd80359627f65bc98b6f35be30
Chris <write2recon@gmail.com>
2023-10-26 17:11:33 +0200

    Update frequencies.c

--- frequencies.c
+++ frequencies.c
@@ -1,4 +1,4 @@
-/* Copyright 2023 Dual Tachyon
+	/* Copyright 2023 Dual Tachyon
  * https://github.com/DualTachyon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,64 +18,108 @@
 #include "misc.h"
 #include "settings.h"
 
-// the BK4819 has 2 bands it covers, 18MHz ~ 630MHz and 760MHz ~ 1300MHz
-const freq_band_table_t BX4819_band1 = { 1800000,  63000000};
-const freq_band_table_t BX4819_band2 = {84000000, 130000000};
 
-const freq_band_table_t frequencyBandTable[7] =
+// the BK4819 has 2 bands it covers, 18MHz ~ 630MHz and 760MHz ~ 1300MHz
+const freq_band_table_t BX4819_BAND1 = { 1800000,  63000000};
+const freq_band_table_t BX4819_BAND2 = {84000000, 130000000};
+
+const freq_band_table_t FREQ_BAND_TABLE[7] =
 {
-	#ifndef ENABLE_WIDE_RX
-		// QS original
-		{.lower =  5000000,  .upper =  7600000},
-		{.lower = 10800000,  .upper = 13700000},
-		{.lower = 13700000,  .upper = 17400000},
-		{.lower = 17400000,  .upper = 35000000},
-		{.lower = 35000000,  .upper = 40000000},
-		{.lower = 40000000,  .upper = 47000000},
-		{.lower = 47000000,  .upper = 60000000}
-	#else
+	#ifdef ENABLE_WIDE_RX
 		// extended range
-		{.lower =  BX4819_band1.lower, .upper =  10800000},
-		{.lower = 10800000, .upper =  13700000},
-		{.lower = 13700000, .upper =  17400000},
-		{.lower = 17400000, .upper =  35000000},
-		{.lower = 35000000, .upper =  40000000},
-		{.lower = 40000000, .upper =  47000000},
-		{.lower = 47000000, .upper = BX4819_band2.upper}
+		{BX4819_BAND1.lower, 10800000},             // band 1
+		{AIR_BAND.lower,     AIR_BAND.upper},       // band 2
+		{AIR_BAND.upper,     17400000},             // band 3
+		{17400000,           35000000},             // band 4
+		{35000000,           40000000},             // band 5
+		{40000000,           47000000},             // band 6
+		{47000000,           BX4819_BAND2.upper}    // band 7
+	#else
+		// QS original
+		{ 5000000,       7600000},         // band 1
+		{AIR_BAND.lower, AIR_BAND.upper},  // band 2
+		{AIR_BAND.upper, 17400000},        // band 3
+		{17400000,       35000000},        // band 4
+		{35000000,       40000000},        // band 5
+		{40000000,       47000000},        // band 6
+		{47000000,       60000000}         // band 7
 	#endif
 };
 
 #ifdef ENABLE_NOAA
-	const uint32_t NoaaFrequencyTable[10] =
+	const uint32_t NOAA_FREQUENCY_TABLE[10] =
 	{
-		44600625,
-		44601875,
-		44603125,
-		44604375,
-		44605625,
-		44606875,
-		44608125,
-		44609375,
-		44610625,
-		44611875
+		16255000,
+		16240000,
+		16247500,
+		16242500,
+		16245000,
+		16250000,
+		16252500,
+		16152500,
+		16177500,
+		16327500
 	};
 #endif
 
-#ifndef ENABLE_12_5KHZ_STEP
-	// QS steps (*10 Hz)
-	const uint16_t StepFrequencyTable[7] = {250, 500, 625, 1000, 1250, 2500, 833};
-#else
-	// includes 1.25kHz step
-	const uint16_t StepFrequencyTable[7] = {125, 250, 625, 1000, 1250, 2500, 833};
-#endif
+// the first 7 values MUST remain in those same positions (to remain compatible with the QS config windows software)
+const uint16_t STEP_FREQ_TABLE[21] = {
+	250, 500, 625, 1000, 1250, 2500, 833,
+	1, 5, 10, 25, 50, 100, 125, 1500, 3000, 5000, 10000, 12500, 25000, 50000
+};
+uint16_t step_freq_table_sorted[ARRAY_SIZE(STEP_FREQ_TABLE)];
 
-FREQUENCY_Band_t FREQUENCY_GetBand(uint32_t Frequency)
+unsigned int FREQUENCY_get_step_index(const unsigned int step_size)
+{	// return the index into 'STEP_FREQ_TABLE' for the supplied step size
+	unsigned int i;
+	for (i = 0; i < ARRAY_SIZE(step_freq_table_sorted); i++)
+		if (STEP_FREQ_TABLE[step_freq_table_sorted[i]] == step_size)
+			return i;
+	// not found, so default to 12.5kHz
+	return 11;
+}
+
+void FREQUENCY_init(void)
+{
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(step_freq_table_sorted); i++)
+		step_freq_table_sorted[i] = i;
+
+	// sort according to step size
+	for (i = 0; i < ARRAY_SIZE(step_freq_table_sorted) - 1; i++)
+	{
+		uint16_t step1 = STEP_FREQ_TABLE[step_freq_table_sorted[i]];
+		unsigned int k;
+		for (k = i + 1; k < ARRAY_SIZE(step_freq_table_sorted); k++)
+		{
+			const uint16_t step2 = STEP_FREQ_TABLE[step_freq_table_sorted[k]];
+			if (step2 < step1)
+			{	// swap
+				const uint16_t temp = step_freq_table_sorted[i];
+				step_freq_table_sorted[i] = step_freq_table_sorted[k];
+				step_freq_table_sorted[k] = temp;
+				step1 = STEP_FREQ_TABLE[step_freq_table_sorted[i]];
+			}
+		}
+	}
+/*
+	#if defined(ENABLE_UART) && defined(ENABLE_UART_DEBUG)
+		UART_SendText("step ..\r\n");
+		for (i = 0; i < ARRAY_SIZE(step_freq_table_sorted); i++)
+			UART_printf("%2u %2u %5u\r\n", i, step_freq_table_sorted[i], STEP_FREQ_TABLE[step_freq_table_sorted[i]]);
+		UART_SendText("\r\n");
+	#endif
+*/
+}
+
+frequency_band_t FREQUENCY_GetBand(uint32_t Frequency)
 {
 	int band;
-	for (band = ARRAY_SIZE(frequencyBandTable) - 1; band >= 0; band--)
-		if (Frequency >= frequencyBandTable[band].lower)
-//		if (Frequency <  frequencyBandTable[band].upper)
-			return (FREQUENCY_Band_t)band;
+	for (band = ARRAY_SIZE(FREQ_BAND_TABLE) - 1; band >= 0; band--)
+		if (Frequency >= FREQ_BAND_TABLE[band].lower && Frequency < FREQ_BAND_TABLE[band].upper)
+//		if (Frequency >= FREQ_BAND_TABLE[band].lower)
+			return (frequency_band_t)band;
 
 	return BAND1_50MHz;
 //	return BAND_NONE;
@@ -83,49 +127,83 @@ FREQUENCY_Band_t FREQUENCY_GetBand(uint32_t Frequency)
 
 uint8_t FREQUENCY_CalculateOutputPower(uint8_t TxpLow, uint8_t TxpMid, uint8_t TxpHigh, int32_t LowerLimit, int32_t Middle, int32_t UpperLimit, int32_t Frequency)
 {
+	uint8_t pwr = TxpMid;
+
 	if (Frequency <= LowerLimit)
 		return TxpLow;
 
-	if (UpperLimit <= Frequency)
+	if (Frequency >= UpperLimit)
 		return TxpHigh;
 
+	// linear interpolation
 	if (Frequency <= Middle)
+		pwr += ((TxpMid  - TxpLow) * (Frequency - LowerLimit)) / (Middle - LowerLimit);
+	else
+		pwr += ((TxpHigh - TxpMid) * (Frequency - Middle))     / (UpperLimit - Middle);
+	return pwr;
+}
+
+uint32_t FREQUENCY_floor_to_step(uint32_t freq, const uint32_t step_size, const uint32_t lower, const uint32_t upper)
+{
+	uint32_t delta;
+
+	if (freq <= lower)
+		return lower;
+
+	if (freq > (upper - 1))
+		freq =  upper - 1;
+
+	delta = freq - lower;
+
+	if (delta < step_size)
+		return lower;
+		
+	if (step_size == 833)
 	{
-		TxpMid += ((TxpMid - TxpLow) * (Frequency - LowerLimit)) / (Middle - LowerLimit);
-		return TxpMid;
+		uint32_t           base  =  (delta / 2500) * 2500;	// 25kHz step
+		const unsigned int index = ((delta - base) % 2500) / step_size;
+
+		if (index == 2)
+			base++;
+
+		freq = lower + base + (step_size * index);
 	}
-
-	TxpMid += ((TxpHigh - TxpMid) * (Frequency - Middle)) / (UpperLimit - Middle);
-
-	return TxpMid;
+	else
+		freq = lower + ((delta / step_size) * step_size);
+	
+	return freq;
 }
 
-static int32_t rnd(int32_t freq, uint16_t step)
+uint32_t FREQUENCY_wrap_to_step_band(uint32_t freq, const uint32_t step_size, const unsigned int band)
 {
-return (freq + (step + 1) / 2) / step * step;
+	const uint32_t upper = FREQ_BAND_TABLE[band].upper; 
+	const uint32_t lower = FREQ_BAND_TABLE[band].lower; 
+	
+	if (freq < lower)
+		return FREQUENCY_floor_to_step(upper, step_size, lower, upper);
+	
+	if (freq >= upper)
+		freq = lower;
+	
+	return freq;
 }
 
-uint32_t FREQUENCY_RoundToStep(uint32_t freq, uint16_t step)
-{
-	if(step == 833) {
-        uint32_t base = freq/2500*2500;
-        int f = rnd(freq - base, step);
-        int x = f / step;
-        return base + f + (x == 3);
-	}
-
-	return rnd(freq, step);
-}
-
-int TX_freq_check(const uint32_t Frequency)
+int FREQUENCY_tx_freq_check(const uint32_t Frequency)
 {	// return '0' if TX frequency is allowed
 	// otherwise return '-1'
 
-	if (Frequency < frequencyBandTable[0].lower || Frequency > frequencyBandTable[ARRAY_SIZE(frequencyBandTable) - 1].upper)
-		return -1;  // not allowed outside this range
+	if (Frequency < BX4819_BAND1.lower || Frequency > BX4819_BAND2.upper)
+		return -1;  // BX radio chip does not work out this range
 
-	if (Frequency >= BX4819_band1.upper && Frequency < BX4819_band2.lower)
-		return -1;  // BX chip does not work in this range
+	if (Frequency >= BX4819_BAND1.upper && Frequency < BX4819_BAND2.lower)
+		return -1;  // BX radio chip does not work in this range
+
+	if (Frequency >= AIR_BAND.lower && Frequency < AIR_BAND.upper)
+		return 0;  // TX not allowed in the airband
+
+	if (Frequency < FREQ_BAND_TABLE[0].lower || Frequency > FREQ_BAND_TABLE[ARRAY_SIZE(FREQ_BAND_TABLE) - 1].upper)
+		return 0;  // TX not allowed outside this range
+
 
 	switch (gSetting_F_LOCK)
 	{
@@ -179,7 +257,7 @@ int TX_freq_check(const uint32_t Frequency)
 			if (Frequency >= 40000000 && Frequency < 43800000)
 				return 0;
 			break;
-		
+
 		#ifdef ENABLE_TX_UNLOCK
 			case FREQ_LOCK_TX_UNLOCK:
 			{
